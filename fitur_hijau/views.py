8e67b92a39333e3b4921e23804d600e01ffb1896
ariefbraja <marief.braja@gmail.com>
2023-05-22 23:58:41 +0700

    fix views.py

--- fitur_hijau/views.py
+++ fitur_hijau/views.py
@@ -27,6 +27,39 @@ curr = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # @login_required("ATLET")
 def ujian_kualifikasi_question(request):
+    if request.method =="POST":
+        question_1  = request.POST.get('q1')
+        question_2  = request.POST.get('q2')
+        question_3  = request.POST.get('q3')
+        question_4  = request.POST.get('q4')
+        question_5  = request.POST.get('q5')
+
+        correct_answers = 0
+        if question_1 == "21":
+            correct_answers += 1
+        if question_2 == "poin":
+            correct_answers += 1
+        if question_3 == "1":
+            correct_answers += 1    
+        if question_4 == "england":
+            correct_answers += 1
+        if question_5 == "kanan":
+            correct_answers += 1
+        # print("jumlah benar : " + str(correct_answers)) 
+
+        id_atlet = request.COOKIES['id']
+        dict_info = request.COOKIES['dict_info']
+        dict = ast.literal_eval(dict_info)
+
+        tahun = dict['tahun']
+        batch = dict['batch']
+        tempat = dict['tempat']
+        tanggal = dict['tanggal']
+        if correct_answers > 3:
+            query = f'UPDATE ATLET_NONKUALIFIKASI_UJIAN_KUALIFIKASI SET hasil_lulus = true WHERE id_atlet = \'{id_atlet}\' AND tahun = \'{tahun}\' AND batch = \'{batch}\' AND tempat = \'{tempat}\' AND tanggal = \'{tanggal}\' AND hasil_lulus = false'
+            print(query)
+            curr.execute(query)
+            conn.commit()
     return render(request, 'ujian_kualifikasi_question.html')
 
 # @login_required("UMPIRE")
@@ -37,8 +70,9 @@ def ujian_kualifikasi_umpire_create(request):
         tempat = request.POST.get('tempat')
         tanggal = request.POST.get('date')
     
-        # query = f'INSERT INTO UJIAN_KUALIFIKASI VALUES (\'{tahun}\', \'{nomor_batch}\', \'{tempat}\, \'{tanggal}\)'
-        # curr.execute(query)
+        query = f'INSERT INTO UJIAN_KUALIFIKASI VALUES(\'{tahun}\', \'{nomor_batch}\', \'{tempat}\', \'{tanggal}\')'
+        curr.execute(query)
+        conn.commit()
     
     return render(request, 'ujian_kualifikasi_umpire_create.html')
 
@@ -60,9 +94,6 @@ def ujian_kualifikasi_umpire_read(request):
 
 @login_required(role="ATLET")
 def ujian_kualifikasi_atlet(request):
-    # tested = '34847a13-05b0-42fa-a5e8-293203691bcf'
-    # queryzz = f'SELECT ANU.id_atlet into id_atlet_sekarang FROM ATLET_NONKUALIFIKASI_UJIAN_KUALIFIKASI AS ANU WHERE id_atlet = \'{tested}\''
-
     query = f'SELECT * FROM UJIAN_KUALIFIKASI'
     curr.execute(query)
     lst = curr.fetchall()
@@ -84,11 +115,15 @@ def ujian_kualifikasi_atlet(request):
         batch = test['batch']
         tempat = test['tempat']
         tanggal = test['tanggal']
-        datetime_object = datetime.strptime(tanggal, "%Y-%m-%d")
+
         try:
-            query = f'INSERT INTO ATLET_NONKUALIFIKASI_UJIAN_KUALIFIKASI VALUES (\'{id_atlet}\', \'{tahun}\', \'{batch}\', \'{tempat}\', \'{tanggal}\')'
+            query = f'INSERT INTO ATLET_NONKUALIFIKASI_UJIAN_KUALIFIKASI VALUES(\'{id_atlet}\', \'{tahun}\', \'{batch}\', \'{tempat}\', \'{tanggal}\', false)'
+            print(query)
             curr.execute(query)
-            return render(request, 'ujian_kualifikasi_question.html')
+            conn.commit()
+            response = HttpResponseRedirect(reverse('fitur_hijau:ujian_kualifikasi_question'))
+            response.set_cookie('dict_info', value)
+            return response
         except Exception as e:
              messages.error(request, generate_error_message(e))
              conn.rollback()
@@ -147,3 +182,6 @@ def riwayat_kualifikasi_umpire(request):
         list_data.append(context)
 
     return render(request, 'riwayat_kualifikasi_umpire.html', {'data' : list_data})
+
+
+
